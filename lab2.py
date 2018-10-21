adc067baeb4a0bd9037870cc270908d775a4c68f
shirr20-meet <shirr20@meet.mit.edu>
2018-10-21 12:26:37 -0400

    what ever

--- lab2.py
+++ lab2.py
@@ -1,13 +1,9 @@
 y= [2,4,6,8,10]
-def thisthing (w):
-	x= [w[0],w[-1]]
-	return x
+z = []
+for x in y:
+	if x <= 5:
+		print(x)
+print(y)
 
-something= thisthing(y)
-print(something)
-s=[1,2,3]
-print(thisthing(s))
-f=[2,3,4,7,18,20]
-for index in f:
-	return 
+
 
