62aa706903420853605d56b155c56509226685b1
shirr20-meet <shirr20@meet.mit.edu>
2019-01-23 12:36:06 -0500

    agario.py

--- agario.py
+++ agario.py
@@ -1,219 +1,146 @@
 import turtle
 import time
 import random
-from personal.project.py import Ball
+import math
+from personal_project import Ball
 
 turtle.tracer(0)
 turtle.hideturtle()
 
-boolean RUNNING set to True
-float SLEEP set to 0.0077
-SCREEN_WIDTH set to turtle.getcanvas().winfo_width()/2
-SCREEN_HEIGHT set to turtle.getcanvas().winfo_height()/2
+RUNNING =True
+SLEEP= 0.0077
+SCREEN_WIDTH =turtle.getcanvas().winfo_width()/2
+SCREEN_HEIGHT = turtle.getcanvas().winfo_height()/2
 
-MY_BALL = Ball(0,0,5,9,15,"pink")
+MY_BALL = Ball(0,0,5,9,30,"pink")
 NUMBER_OF_BALLS = 5
 MINIMUM_BALL_RADIUS = 10
-MAXIMUM_BALL_RADIUS = 100
-MINIMUM_BALL_DX = 0
-MAXIMUM_BALL_DX = 0
-MINIMUM_BALL_DY = 0
-MAXIMUM_BALL_DY = 0
+MAXIMUM_BALL_RADIUS = 65
+MINIMUM_BALL_DX = -5
+MAXIMUM_BALL_DX = 5
+MINIMUM_BALL_DY = -5
+MAXIMUM_BALL_DY = 5
 
 BALLS= []
 
-for BALLS in range (5):
-	x = random.randit(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS, SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
-	y = random.randit(-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS , SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
-	dx = random.randit(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
-	dy = random.randit(MINIMUM_BALL_DY ,  MAXIMUM_BALL_DY)
-	r = random.randit(MINIMUM_BALL_RADIUS , MAXIMUM_BALL_RADIUS)
+for B in range (NUMBER_OF_BALLS):
+	x = random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS, SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
+	y = random.randint(-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS , SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
+	dx = random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+	dy = random.randint(MINIMUM_BALL_DY ,  MAXIMUM_BALL_DY)
+	r = random.randint(MINIMUM_BALL_RADIUS , MAXIMUM_BALL_RADIUS)
 	color= (random.random(), random.random(), random.random())
 
-ball = Ball(x,y,dx,dy,r,color)
-BALLS.append(ball)
+	ball = Ball(x,y,dx,dy,r,color)
+	BALLS.append(ball)
 
 #making all balls move
 def move_all_balls():
 	for ball in BALLS :
-		ball.move (SCREEN_WIDTH,SCREEN_HEIGHT)
+		ball.move(SCREEN_WIDTH,SCREEN_HEIGHT)
 
 def collide(ball_a , ball_b):
-	if(ball_a == ball_b):False
-		math.sqrt(math.pow((-x1),2) + math.pow((y2-y1),2))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	if(ball_a == ball_b):
+		return False
+	d=math.sqrt(math.pow((ball_b.xcor()-ball_a.xcor()),2) + math.pow(ball_b.ycor()-ball_a.ycor(),2))
+	if d <= ball_a.r+ ball_b.r: 
+		return True
+	else :
+		return False
+
+def  check_all_balls_collision():
+
+	for ball_a in BALLS:
+		for ball_b in BALLS :
+			if collide(ball_a, ball_b):
+				r1= ball_a.r
+				r2= ball_b.r
+				
+				x = random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS, SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
+				y = random.randint(-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS , SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
+				dx = random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+				if dx ==0 :
+					dx= random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+
+				dy = random.randint(MINIMUM_BALL_DY ,  MAXIMUM_BALL_DY)
+				if dy ==0:
+					dy = random.randint(MINIMUM_BALL_DY ,  MAXIMUM_BALL_DY)
+
+				r = random.randint(MINIMUM_BALL_RADIUS , MAXIMUM_BALL_RADIUS)
+				color= (random.random(), random.random(), random.random())
+				
+				if r1>r2:
+					r1+=1
+					ball_a.r= r1
+					ball_a.shapesize(r1/10)
+					ball_b.goto(x,y)
+					ball_b.dx=dx
+					ball_b.dy=dy
+					ball_b.r=r
+					ball_b.color() 
+					ball_b.shapesize(r/10)
+				else:
+					r2+=1
+					ball_b.r= r2
+					ball_b.shapesize(r2/10)
+					ball_a.goto(x,y)
+					ball_a.dx=dx
+					ball_a.dy=dy
+					ball_a.r=r
+					ball_a.color() 
+					ball_a.shapesize(r/10)
+
+def check_myball_collision():
+	global MY_BALL
+	for ball in BALLS :
+		if collide(MY_BALL,ball):
+			r3= MY_BALL.r
+			r4= ball.r
+			x = random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS, SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
+			y = random.randint(-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS , SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
+			dx = random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+			if dx ==0 :
+				dx= random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+
+			dy = random.randint(MINIMUM_BALL_DY ,  MAXIMUM_BALL_DY)
+			if dy ==0:
+				dy = random.randint(MINIMUM_BALL_DY ,  MAXIMUM_BALL_DY)
+
+			r = random.randint(MINIMUM_BALL_RADIUS , MAXIMUM_BALL_RADIUS)
+			color= (random.random(), random.random(), random.random())
+			
+
+
+			if r3<r4:
+				return False
+			else:
+				r3+=1
+				MY_BALL.r = r3
+				MY_BALL.shapesize(r3/10)
+				ball.goto(x,y)
+				ball.dx=dx
+				ball.dy=dy
+				ball.r=r
+				ball.color() 
+				ball.shapesize(r/10)
+	return True
+
+def movearound(event):
+	MY_BALL.goto(event.x - SCREEN_WIDTH, SCREEN_HEIGHT- event.y)
+
+turtle.getcanvas().bind("<Motion>", movearound)
+	
+turtle.listen()
+
+
+while RUNNING: 
+	SCREEN_WIDTH =turtle.getcanvas().winfo_width()/2
+	SCREEN_HEIGHT = turtle.getcanvas().winfo_height()/2
+	turtle.update()
+	move_all_balls()
+	check_all_balls_collision()
+	RUNNING = check_myball_collision()
+	time.sleep(SLEEP)
 
 
 
---
+++ personal_project.py
@@ -0,0 +1,43 @@
+from turtle import * 
+import turtle
+
+class Ball(Turtle):
+	"""docstring for Ball"""
+	def __init__(self,x,y,dx,dy,r,color):
+		Turtle.__init__(self)
+		self.x = x
+		self.y = y
+		self.dx = dx
+		self.dy = dy
+		self.r = r
+		self.penup()
+
+		
+		
+		self.goto(x,y)
+		self.shape("circle")
+		self.shapesize(r/10)
+		self.color(color)
+
+	def move(self, screen_width, screen_height):
+
+		current_x= self.xcor()
+		new_x = current_x + self.dx
+		current_y= self.ycor()
+		new_y= current_y + self.dy
+		right_side_ball = new_x + self.r
+		left_side_ball = new_x - self.r
+		top_side_ball = new_y + self.r
+		bottom_side_ball= new_y - self.r
+		self.goto(new_x, new_y)
+		if right_side_ball>screen_width:
+			self.dx = -self.dx
+		if left_side_ball< -screen_width:
+			self.dx = -self.dx
+		if top_side_ball> screen_height:
+			self.dy = -self.dy
+		if bottom_side_ball< -screen_height:
+			self.dy= -self.dy
+	
+
+		
